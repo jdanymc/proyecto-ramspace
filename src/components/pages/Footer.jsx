8eb1cf3c70bbb4568977fc585881e0285a5e95a0
Jhon Dany Mamani <jdany.mc@gmail.com>
2023-05-04 23:36:06 -0500

    carrito rapido avance

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,35 +1,42 @@
+import CarritoRapido from "../cart";
+
 const footer = () => {
   return (
-    <footer>
-    <div className="footer">
-      <div className="footer_datos">
-        <p>Horarios de atención</p>
-        <small>Lun a Vie: 8am - 8pm</small>
-      </div>
-      <div className="footer_datos">
-        <p>Gmail</p>
-        <small>ls@gmail.com</small>
-      </div>
-      <div className="footer_datos">
-        <p>Redes</p>
-        <div className="datos_icon">
-          <img
-            src="./img/logo/whatsapp_icon-icons.com_53606.png"
-            alt="icon Whatsapp"
-          />
-          <img
-            src="./img/logo/facebook_icon-icons.com_53612.png"
-            alt="icon Facebook"
-          />
-          <img
-            src="./img/logo/Instagram_icon-icons.com_66804.png"
-            alt="icon instagram"
-          />
+    <>
+      <footer>
+        <div className="footer">
+          <div className="footer_datos">
+            <p>Horarios de atención</p>
+            <small>Lun a Vie: 8am - 8pm</small>
+          </div>
+          <div className="footer_datos">
+            <p>Gmail</p>
+            <small>ls@gmail.com</small>
+          </div>
+          <div className="footer_datos">
+            <p>Redes</p>
+            <div className="datos_icon">
+              <img
+                src="./img/logo/whatsapp_icon-icons.com_53606.png"
+                alt="icon Whatsapp"
+              />
+              <img
+                src="./img/logo/facebook_icon-icons.com_53612.png"
+                alt="icon Facebook"
+              />
+              <img
+                src="./img/logo/Instagram_icon-icons.com_66804.png"
+                alt="icon instagram"
+              />
+            </div>
+          </div>
         </div>
+      </footer>
+      <div id="modal">
+        <CarritoRapido />
       </div>
-    </div>
-  </footer>
-  )
-}
+    </>
+  );
+};
 
-export default footer
+export default footer;
